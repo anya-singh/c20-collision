ca38b86d8340c2eef4ad118eaa6c92bd703e3ce0
anya-singh <69683960+anya-singh@users.noreply.github.com>
2020-08-21 16:31:04 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var movingRect,fixedRect
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 50, 50);
+  movingRect = createSprite(100,100,30,80);
+  fixedRect.shapeColor = "red"; 
+  movingRect.shapeColor = "red";
+  fixedRect.debug = true ; 
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(0); 
+  
+  movingRect.x = World.mouseX ; 
+  movingRect.y = World.mouseY ; 
+  if(fixedRect.x - movingRect.x <= fixedRect.width/2 + movingRect.width/2 && 
+    movingRect.x - fixedRect.x <= fixedRect.width/2 + movingRect.width/2 &&
+    fixedRect.y- movingRect.y <= fixedRect.height/2 + movingRect.height/2 && 
+    movingRect.y - fixedRect.y <= fixedRect.height/2 + movingRect.height/2 ){
+    fixedRect.shapeColor = "yellow"; 
+    movingRect.shapeColor = "yellow";
+  }
+  else{
+    fixedRect.shapeColor = "red"; 
+    movingRect.shapeColor = "red";
+  }
+
+  drawSprites();
+}
